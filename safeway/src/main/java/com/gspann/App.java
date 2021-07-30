5331bacc58d5c1a24f7b08fcb0c60874fe08cd64
swamychikala <swamy550@gmail.com>
2021-07-30 12:16:48 +0000

    Maven

---
+++ safeway/src/main/java/com/gspann/App.java
@@ -0,0 +1,13 @@
+package com.gspann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
